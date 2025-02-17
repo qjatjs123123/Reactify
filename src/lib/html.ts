6e89a670b65ab3fa5615b5eb6a7f444d325a5a11
홍범선 <qjatjs123123@naver.com>
2025-02-18 01:01:40 +0900

    refactor: 함수명 수정

--- src/lib/html.ts
+++ src/lib/html.ts
@@ -14,27 +14,31 @@ class Tmpl {
       case value instanceof Tmpl:
         return (value as Tmpl).toHtml();
       case Array.isArray(value):
-        return this._mergeArray(value).toHtml();
+        return this._mergeArray(value);
       default:
         return value;
     }
   }
 
-  private _mergeArray = (value: unknown) =>
-    Array.isArray(value)
-      ? value.reduce(
-          (a, b) =>
-            html`<div class="isArray">
-              ${a}
-              <div class="isArray">${b}</div>
-            </div>`,
-          html`<div class="isArray"></div>`
-        )
-      : value;
+  private _mergeArray = (value: unknown) => {
+    if (Array.isArray(value)) {
+      const divEl = document.createElement('div') as HTMLDivElement;
+      divEl.classList.add('isArray');
+
+      value.forEach(item => {
+        const htmlElement = this._parseValue(item);
+        divEl.appendChild(htmlElement); 
+      });
+  
+      return divEl;
+    }
+  
+    return value;
+  };
 
   toHtml(): HTMLElement {
-    const [wrapEl, nodeList, funcList] = this.find();
-    this.replace(wrapEl, nodeList);
+    const [wrapEl, nodeList, funcList] = this.buildHTMLContent();
+    this.replaceNode(wrapEl, nodeList);
     this.eventBind(wrapEl, funcList)
     
     return wrapEl.firstElementChild as HTMLElement;
@@ -53,13 +57,39 @@ class Tmpl {
   }
   
 
-  private replace(wrapEl: HTMLElement, nodeList: Map<number, HTMLElement>) {
+  private replaceNode(wrapEl: HTMLElement, nodeList: Map<number, HTMLElement>) {
     nodeList.forEach((node, id) => {
       wrapEl.querySelector(`#custom-id-${id}`)?.replaceWith(node);
     });
   }
 
-  private find(): [HTMLElement, Map<number, HTMLElement>, any[]] {
+  private _stringifyValue(value: unknown, funcList, nodeList, str: string): string {
+    if (value instanceof Function) {
+      return this._handleFunction(value, funcList, str);
+    } else if (value instanceof HTMLElement) {
+      return this._handleHTMLElement(value, nodeList, str);
+    } else {
+      return str + value;
+    }
+  }
+  
+  private _handleFunction(value: Function, funcList: any[], str: string): string {
+    const funcId = funcList.length;
+    const arr = str.split(" ");
+    const eventType = arr.pop()?.split("=")[0];
+    str = arr.join(" ");
+  
+    funcList.push([value, funcId, eventType]);
+    return str + ` data-func-id=${funcId}`;
+  }
+  
+  private _handleHTMLElement(value: HTMLElement, nodeList: Map<number, HTMLElement>, str: string): string {
+    const nodeId = nodeList.size;
+    nodeList.set(nodeId, value);
+    return str + `<div id="custom-id-${nodeId}"></div>`;
+  }
+
+  private buildHTMLContent(): [HTMLElement, Map<number, HTMLElement>, any[]] {
     const wrapEl = document.createElement("div");
     const nodeList: Map<number, HTMLElement> = new Map();
     const funcList: any = []
@@ -67,24 +97,7 @@ class Tmpl {
     let template = "";
     [...this.strings].forEach((str, i) => {
       const newValue = this._parseValue(this.values[i] ?? "");
-      
-
-      if (newValue instanceof Function) {
-        const funcId = funcList.length
-        let arr = str.split(" ");
-        const eventType = arr.pop()?.split("=")[0];
-        str = arr.join(" ");
-        
-        funcList.push([newValue, funcId, eventType]);
-        template += str +  ` data-func-id=${funcId}`;
-      } else if (newValue instanceof HTMLElement) {
-        const nodeId = nodeList.size;
-        nodeList.set(nodeId, newValue);
-        template += str + `<div id="custom-id-${nodeId}"></div>`;
-      } else {
-        template += str + newValue; 
-      }
-
+      template += this._stringifyValue(newValue, funcList, nodeList, str);
     });
 
     wrapEl.innerHTML = template;
